1dd56b7df6af666737101f7e416447d90ac17908
Felix Putera <felixputera@gmail.com>
2018-09-09 06:28:30 +0800

    Flappy burd

---
+++ Assets/FlappyBirdMinigame/StartScreenScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartScreenScript : MonoBehaviour {
+
+	static bool sawOnce = false;
+    static bool start = false;
+
+    public static void Init() {
+        start = true;
+    }
+
+	// Use this for initialization
+	void Start () {
+		if(!sawOnce) {
+			GetComponent<SpriteRenderer>().enabled = true;
+			Time.timeScale = 0;
+		}
+
+		sawOnce = true;
+	}
+	
+	// Update is called once per frame
+	public void Update () {
+        if(Time.timeScale==0 && start ) {
+			Time.timeScale = 1;
+			GetComponent<SpriteRenderer>().enabled = false;
+
+		}
+	}
+}
---
+++ Assets/Scripts/FlappyBirdScreen.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlappyBirdScreen : MonoBehaviour {
+
+    public Camera cam;
+	// Use this for initialization
+	void Start () {
+        //cam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+        Debug.Log(cam.name);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0)) {
+            RaycastHit hitInfo = new RaycastHit();
+            if (Physics.Raycast(cam.ScreenPointToRay(Input.mousePosition), out hitInfo))
+            {
+                Debug.Log("Object Hit is " + hitInfo.collider.gameObject.name);
+
+                //If you want it to only detect some certain game object it hits, you can do that here
+                if (ReferenceEquals(hitInfo.collider.gameObject, gameObject))
+                {
+                    StartScreenScript.Init();
+                }
+            }
+
+        }
+    }
+}
