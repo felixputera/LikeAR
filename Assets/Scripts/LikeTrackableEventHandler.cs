3a04b7279efeb7f06d43fec33f4befb44152833b
Kevin Luvian <kluvian001@e.ntu.edu.sg>
2018-09-08 23:59:56 +0800

    add API integration

--- Assets/Scripts/LikeTrackableEventHandler.cs
+++ Assets/Scripts/LikeTrackableEventHandler.cs
@@ -1,83 +1,142 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Networking;
 using Vuforia;
 
-public class LikeTrackableEventHandler : DefaultTrackableEventHandler {
+public class LikeTrackableEventHandler : DefaultTrackableEventHandler
+{
+	public GameObject canvas;
+	Coroutine httpCoroutine;
 
-    public GameObject canvas;
-    public GameObject loader;
+	VuMarkManager m_VuMarkManager;
+	VuMarkTarget m_ClosestVuMark;
+	VuMarkTarget m_CurrentVuMark;
 
-    VuMarkManager m_VuMarkManager;
-    VuMarkTarget m_ClosestVuMark;
-    VuMarkTarget m_CurrentVuMark;
+	[System.Serializable]
+	public class Target
+	{
+		public string title;
+		public string description;
+		public int like_count;
+		public string like_count_humanize;
+	}
+	[System.Serializable]
+	public class Friend
+	{
+		public int id;
+		public string name;
+	}
+	[System.Serializable]
+	public class LikedBy
+	{
+		public Friend[] friends;
+		public int rest_likes_count;
+	}
+	[System.Serializable]
+	public class StickerSummary
+	{
+		public Target target;
+		public bool is_liked;
+		public LikedBy liked_by;
+	}
+	[System.Serializable]
+	public class StickerSummaryResponse
+	{
+		public bool is_success;
+		public StickerSummary data;
+	}
 
+	// Use this for initialization
+	protected override void Start()
+	{
+		base.Start();
+		m_VuMarkManager = TrackerManager.Instance.GetStateManager().GetVuMarkManager();
+		m_VuMarkManager.RegisterVuMarkDetectedCallback(OnVuMarkDetected);
+		m_VuMarkManager.RegisterVuMarkLostCallback(OnVuMarkLost);
+		Debug.Log("Start OMEGALUL");
 
-    // Use this for initialization
-    protected override void Start () {
-        base.Start();
-        m_VuMarkManager = TrackerManager.Instance.GetStateManager().GetVuMarkManager();
-        m_VuMarkManager.RegisterVuMarkDetectedCallback(OnVuMarkDetected);
-        m_VuMarkManager.RegisterVuMarkLostCallback(OnVuMarkLost);
-        Debug.Log("Start OMEGALUL");
+	}
 
-    }
+	#region PUBLIC_METHODS
 
-    #region PUBLIC_METHODS
+	/// <summary>
+	/// This method will be called whenever a new VuMark is detected
+	/// </summary>
+	public void OnVuMarkDetected(VuMarkTarget target)
+	{
+		Debug.Log("New VuMark: " + target.InstanceId.NumericValue);
+		if (httpCoroutine != null) StopCoroutine(httpCoroutine);
+		gameObject.SetActive(true);
+		httpCoroutine = StartCoroutine("FetchFromBackend", (int)target.InstanceId.NumericValue);
+	}
 
-    /// <summary>
-    /// This method will be called whenever a new VuMark is detected
-    /// </summary>
-    public void OnVuMarkDetected(VuMarkTarget target)
-    {
-        Debug.Log("New VuMark: " + target.InstanceId.NumericValue);
-        StartCoroutine(FetchFromBackend((int) target.InstanceId.NumericValue));
-    }
+	/// <summary>
+	/// This method will be called whenever a tracked VuMark is lost
+	/// </summary>
+	public void OnVuMarkLost(VuMarkTarget target)
+	{
+		Debug.Log("Lost VuMark: " + GetVuMarkId(target));
+	}
 
-    /// <summary>
-    /// This method will be called whenever a tracked VuMark is lost
-    /// </summary>
-    public void OnVuMarkLost(VuMarkTarget target)
-    {
-        Debug.Log("Lost VuMark: " + GetVuMarkId(target));
+	#endregion // PUBLIC_METHODS
 
-    }
+	string GetVuMarkId(VuMarkTarget vumark)
+	{
+		switch (vumark.InstanceId.DataType)
+		{
+			case InstanceIdType.BYTES:
+				return vumark.InstanceId.HexStringValue;
+			case InstanceIdType.STRING:
+				return vumark.InstanceId.StringValue;
+			case InstanceIdType.NUMERIC:
+				return vumark.InstanceId.NumericValue.ToString();
+		}
+		return string.Empty;
+	}
 
-    #endregion // PUBLIC_METHODS
+	private IEnumerator FetchFromBackend(int id)
+	{
+		UnityWebRequest www = UnityWebRequest.Get("https://likeplus.ntusu.org/api/stickers/" + id);
+		yield return www.SendWebRequest();
 
-    string GetVuMarkId(VuMarkTarget vumark)
-    {
-        switch (vumark.InstanceId.DataType)
-        {
-            case InstanceIdType.BYTES:
-                return vumark.InstanceId.HexStringValue;
-            case InstanceIdType.STRING:
-                return vumark.InstanceId.StringValue;
-            case InstanceIdType.NUMERIC:
-                return vumark.InstanceId.NumericValue.ToString();
-        }
-        return string.Empty;
-    }
+		if (www.isNetworkError || www.isHttpError)
+		{
+			Debug.Log("https://likeplus.ntusu.org/api/stickers/" + id);
+			Debug.Log(www.error);
+		}
+		else
+		{
+			// Hide loader
+			//loader.SetActive(false);
 
-    private IEnumerator FetchFromBackend(int id) {
-        UnityWebRequest www = UnityWebRequest.Get("http://www.example.com/" + id);
-        yield return www.SendWebRequest();
+			// Show results as text
+			StickerSummaryResponse resp = JsonUtility.FromJson<StickerSummaryResponse>(www.downloadHandler.text);
+			Debug.Log(resp.data);
+			Debug.Log(www.downloadHandler.text);
+			Debug.Log(resp.data.target.title);
 
-        if (www.isNetworkError || www.isHttpError)
-        {
-            Debug.Log(www.error);
-        }
-        else
-        {
-            // Hide loader
-            loader.SetActive(false);
+			GameObject titleObject = GameObject.FindGameObjectWithTag("Title");
+			titleObject.GetComponent<UnityEngine.UI.Text>().text = resp.data.target.title;
 
-            // Show results as text
-            Debug.Log(www.downloadHandler.text);
+			GameObject descriptionObject = GameObject.FindGameObjectWithTag("Description");
+			descriptionObject.GetComponent<UnityEngine.UI.Text>().text = resp.data.target.description;
 
-            // Or retrieve results as binary data
-            byte[] results = www.downloadHandler.data;
-        }
-    }
+			if (resp.data.is_liked) {
+				GameObject likeButton = GameObject.FindGameObjectWithTag("LikeButton");
+				GameObject notLikedButton = GameObject.FindGameObjectWithTag("NotLikedButton");
+				likeButton.SetActive(true);
+				notLikedButton.SetActive(false);
+			} else {
+				GameObject likeButton = GameObject.FindGameObjectWithTag("LikeButton");
+				GameObject notLikedButton = GameObject.FindGameObjectWithTag("NotLikedButton");
+				likeButton.SetActive(false);
+				notLikedButton.SetActive(true);
+			}
+
+			// Or retrieve results as binary data
+			byte[] results = www.downloadHandler.data;
+		}
+	}
 }
