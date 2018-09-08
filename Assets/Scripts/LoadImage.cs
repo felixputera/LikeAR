809b2daffbcbf8247ff7aacbf4c729a11fdc3e39
Albert Lucianto <albertlucianto@gmail.com>
2018-09-09 03:20:50 +0800

    update like icon and add load image script

---
+++ Assets/Scripts/LoadImage.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadImage : MonoBehaviour
+{
+    // Use this for initialization
+    IEnumerator SetImage(string imageUrl)
+    {
+        // Start a download of the given URL
+        using (WWW www = new WWW(imageUrl))
+        {
+            // Wait for download to complete
+            yield return www;
+            Texture2D tex = www.texture;
+
+            // assign texture
+            Sprite newSprite = Sprite.Create(tex, new Rect(0.0f, 0.0f, tex.width, tex.height), new Vector2(0.5f, 0.5f), 100.0f);
+            GetComponent<UnityEngine.UI.Image>().sprite = newSprite;
+        }
+    }
+}
